0193595e4b65bf896d8a5c84378d40b69a732ff6
MYEONGJUNG\MJ Park <92mj.park@gmail.com>
2021-08-25 08:40:55 +0900

    add week 3

---
+++ Weekly Challenge/Week 3.py
@@ -0,0 +1,78 @@
+문제 설명
+테이블 위에 놓인 퍼즐 조각을 게임 보드의 빈 공간에 적절히 올려놓으려 합니다. 게임 보드와 테이블은 모두 각 칸이 1x1 크기인 정사각 격자 모양입니다. 이때, 다음 규칙에 따라 테이블 위에 놓인 퍼즐 조각을 게임 보드의 빈칸에 채우면 됩니다.
+
+조각은 한 번에 하나씩 채워 넣습니다.
+조각을 회전시킬 수 있습니다.
+조각을 뒤집을 수는 없습니다.
+게임 보드에 새로 채워 넣은 퍼즐 조각과 인접한 칸이 비어있으면 안 됩니다.
+다음은 퍼즐 조각을 채우는 예시입니다.
+
+puzzle_5.png
+
+위 그림에서 왼쪽은 현재 게임 보드의 상태를, 오른쪽은 테이블 위에 놓인 퍼즐 조각들을 나타냅니다. 테이블 위에 놓인 퍼즐 조각들 또한 마찬가지로 [상,하,좌,우]로 인접해 붙어있는 경우는 없으며, 흰 칸은 퍼즐이 놓이지 않은 빈 공간을 나타냅니다. 모든 퍼즐 조각은 격자 칸에 딱 맞게 놓여있으며, 격자 칸을 벗어나거나, 걸쳐 있는 등 잘못 놓인 경우는 없습니다.
+
+이때, 아래 그림과 같이 3,4,5번 조각을 격자 칸에 놓으면 규칙에 어긋나므로 불가능한 경우입니다.
+
+puzzle_6.png
+
+3번 조각을 놓고 4번 조각을 놓기 전에 위쪽으로 인접한 칸에 빈칸이 생깁니다.
+5번 조각의 양 옆으로 인접한 칸에 빈칸이 생깁니다.
+다음은 규칙에 맞게 최대한 많은 조각을 게임 보드에 채워 넣은 모습입니다.
+
+puzzle_7.png
+
+최대한 많은 조각을 채워 넣으면 총 14칸을 채울 수 있습니다.
+
+현재 게임 보드의 상태 game_board, 테이블 위에 놓인 퍼즐 조각의 상태 table이 매개변수로 주어집니다. 규칙에 맞게 최대한 많은 퍼즐 조각을 채워 넣을 경우, 총 몇 칸을 채울 수 있는지 return 하도록 solution 함수를 완성해주세요.
+
+입출력 예
+game_board	table	result
+[[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]]	[[1,0,0,1,1,0],[1,0,1,0,1,0],[0,1,1,0,1,1],[0,0,1,0,0,0],[1,1,0,1,1,0],[0,1,0,0,0,0]]	14
+[[0,0,0],[1,1,0],[1,1,1]]	[[1,1,1],[1,0,0],[0,0,0]]	0
+
+=================================================================================
+
+from collections import deque
+
+def find(board, num):
+    N = len(board)
+    block = {}
+    cnt = 0
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == num:
+                block[cnt] = [(i,j)]
+                q = deque([(i,j)])
+                board[i][j] = num^1
+                while q:
+                    x, y = q.popleft()
+                    for dx, dy in ((1,0), (-1,0), (0,1), (0,-1)):
+                        nx, ny = x+dx, y+dy
+                        if (0 <= nx < N) and (0 <= ny < N) and board[nx][ny] == num:
+                            block[cnt].append((nx, ny))
+                            q.append((nx, ny))
+                            board[nx][ny] = num^1
+                block[cnt].sort()
+                cnt += 1
+    return block
+
+
+def solution(game_board, table):
+    answer = 0
+    block = find(table, 1) 
+    frame = find(game_board, 0)
+    completed = set()
+    for i in block:
+        for j in frame:
+            if (j not in completed) and (len(block[i]) == len(frame[j])):
+                rotated = block[i]
+                for _ in range(4):
+                    rotated = sorted([(-y, x) for x, y in rotated])
+                    diff = set([(p[0]-q[0], p[1]-q[1]) for p, q in zip(rotated, frame[j])])
+                    if len(diff) == 1:
+                        answer += len(frame[j])
+                        completed.add(j)
+                        break
+                if len(diff) == 1:
+                    break
+    return answer
